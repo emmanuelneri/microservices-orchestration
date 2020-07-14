44ebce08db130c91285814a51a7fd00ff79a8d44
emmanuelneri <emmanuelnerisouza@gmail.com>
2020-07-14 20:27:29 -0300

    simplify scripts

--- create-kafka-topics.sh
+++ create-kafka-topics.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
 namespace=kafka
-kafka=($(kubectl get pods -n ${namespace} -l 'app=cp-kafka' \
-    -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')*/)
-
-zookeeper=($(kubectl get pods -n ${namespace} -l 'app=cp-zookeeper' \
-    -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')*/)
-
-zookeepers=$(sed 's/.\{2\}$//'<<< $zookeeper)":2181"
+kafka=confluent-oss-cp-kafka-0
+zookeeper=confluent-oss-cp-zookeeper:2181
 
 echo "-------------- Kafka Create Topics --------------"
 echo "kafka selected: ${kafka}"
-echo "zookeeper: ${zookeepers}"
+echo "zookeeper: ${zookeeper}"
 
-kubectl exec -c cp-kafka-broker -it ${kafka} -n ${namespace} -- /bin/bash /usr/bin/kafka-topics --create --topic ApiRequested --partitions 3 --replication-factor 3 --if-not-exists --zookeeper ${zookeepers}
+kubectl exec -c cp-kafka-broker -it ${kafka} -n ${namespace} -- /bin/bash /usr/bin/kafka-topics --create --topic ApiRequested --partitions 3 --replication-factor 3 --if-not-exists --zookeeper ${zookeeper}
