ae58f456cc9e809f755e6acb6b8c4acd655ae53f
emmanuelneri <emmanuelnerisouza@gmail.com>
2020-07-16 23:54:56 -0300

    change autoscaling

--- create-kafka-topics.sh
+++ create-kafka-topics.sh
@@ -8,4 +8,4 @@ echo "-------------- Kafka Create Topics --------------"
 echo "kafka selected: ${kafka}"
 echo "zookeeper: ${zookeeper}"
 
-kubectl exec -c cp-kafka-broker -it ${kafka} -n ${namespace} -- /bin/bash /usr/bin/kafka-topics --create --topic ApiRequested --partitions 3 --replication-factor 3 --if-not-exists --zookeeper ${zookeeper}
+kubectl exec -c cp-kafka-broker -it ${kafka} -n ${namespace} -- /bin/bash /usr/bin/kafka-topics --create --topic ApiRequested --partitions 6 --replication-factor 3 --if-not-exists --zookeeper ${zookeeper}
--- stop-all.sh
+++ stop-all.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 echo "-------------- Starting stop Cluster --------------"
-kubectl -n kafka delete pod,service,deployment --all
-helm delete -n kafka $(helm -n kafka ls --short)
 kubectl -n apps delete pod,service,deployment,ingress,hpa --all
 kubectl -n ingress-nginx delete pod,service,deployment --all
+
+helm delete -n kafka $(helm -n kafka ls --short)
+kubectl delete statefulset --all -n kafka
+kubectl delete pvc --all -n kafka
