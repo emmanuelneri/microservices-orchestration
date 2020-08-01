a0bc2648bf5f0bc4e488f00c5054bc1a5e77763a
emmanuelneri <emmanuelnerisouza@gmail.com>
2020-08-01 16:16:40 -0300

    centralize avro

---
+++ commons/avro.go
@@ -0,0 +1,15 @@
+package commons
+
+import (
+	"github.com/linkedin/goavro/v2"
+	"io/ioutil"
+)
+
+func LoadAvroCodec(filename string) (*goavro.Codec, error) {
+	schema, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	return goavro.NewCodec(string(schema))
+}
